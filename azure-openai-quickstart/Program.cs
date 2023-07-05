ed276045e841f1e730a6b5195c0a2337500c6d1a
Gaurav Sarawgi <gsarawgi@cs.ad.1worldsync.com>
2023-07-05 14:43:23 +0530

    First OpenAI commit

---
+++ azure-openai-quickstart/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure;
+using Azure.AI.OpenAI;
+using static System.Environment;
+
+string endpoint = GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT");
+string key = GetEnvironmentVariable("AZURE_OPENAI_KEY");
+
+// Enter the deployment name you chose when you deployed the model.
+string engine = "text-davinci-003-deploy";
+
+OpenAIClient client = new(new Uri(endpoint), new AzureKeyCredential(key));
+
+string prompt = "When was Microsoft founded?";
+Console.Write($"Input: {prompt}\n");
+
+Response<Completions> completionsResponse = 
+    await client.GetCompletionsAsync(engine, prompt);
+string completion = completionsResponse.Value.Choices[0].Text;
+Console.WriteLine($"Chatbot: {completion}");
